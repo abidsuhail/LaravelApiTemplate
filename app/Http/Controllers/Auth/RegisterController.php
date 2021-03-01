c08a86a931fa18a5260ef022a58b7c509a8953db
Abid Suhail <abid1294005@gmail.com>
2021-03-01 16:53:55 +0530

    register error working

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use Exception;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -65,11 +66,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+    
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+       
+      
     }
     protected function registered(Request $request, $user)
     {
